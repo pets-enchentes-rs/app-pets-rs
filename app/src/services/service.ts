4a5916d834c2f225fc76111ad34660dd671afb0a
Fernando Halmenschlager Filter <fernando.filter@universo.univates.br>
2024-06-12 20:33:12 -0300

    Changed API Path

--- app/src/services/service.ts
+++ app/src/services/service.ts
@@ -1,13 +1,11 @@
 import axios from 'axios'
 
-const port = process.env.API_PORT ?? 8080
-
 const instance = axios.create({
-  baseURL: `http://localhost:${port}`
+  baseURL: process.env.API_PATH
 })
 
 export default {
-  async get<T>(url: string, params = {}, options = {}): Promise<any> {
+  async get(url: string, params = {}, options = {}): Promise<any> {
     const { data } = await instance.get(url, {
       params,
       ...options
@@ -16,15 +14,15 @@ export default {
     return data
   },
 
-  async post<T>(url: string, data: object): Promise<any> {
+  async post(url: string, data: object): Promise<any> {
     return instance.post(url, data)
   },
 
-  async put<T>(url: string, data: object): Promise<any> {
+  async put(url: string, data: object): Promise<any> {
     return instance.put(url, data)
   },
 
-  async delete<T>(url: string): Promise<any> {
+  async delete(url: string): Promise<any> {
     return instance.delete(url)
   }
 }
